aebb9cf0c9d384a6a6d6e0b45507d1ab1881d6a4
LegendaryJoseph <125080328+LegendaryJoseph@users.noreply.github.com>
2023-10-09 16:45:43 +0300

    auth working successfully

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
-function App() {
+function App({ signOut, user }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from Vee</h1>
-      </header>
-    </div>
+    <>
+      <h1>Hello {user.username}</h1>
+      <button onClick={signOut}>Sign out</button>
+    </>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
